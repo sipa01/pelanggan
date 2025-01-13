9244096c9d470e590e8899dc89a8854708d1d649
mahda07 <mahdafatihah@gmail.com>
2025-01-13 10:06:43 +0700

    menambah config firebase punya saya sendiri

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ import {
   query,
   orderBy
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfqZD7UZZt-GWmtNhfJyksrv3-8ENRjto",
+  authDomain: "insan-cemerlang-d5574.firebaseapp.com",
+  projectId: "insan-cemerlang-d5574",
+  storageBucket: "insan-cemerlang-d5574.appspot.com",
+  messagingSenderId: "1035937160050",
+  appId: "1:1035937160050:web:6d77d3874c3f78b2811beb",
+  measurementId: "G-EVVQ80Q08C"
+};
+
